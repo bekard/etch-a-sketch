6d7be91c028bb2cfa30e318a8c2e90fe182fd925
ant.ar9809 <ant.ar9809@gmail.com>
2022-04-03 21:43:42 +0300

    [add] reset of the grid and its creation with some user size

--- main.js
+++ main.js
@@ -1,31 +1,59 @@
-const GRID_SIZE = 32;
+const DEFAULT_GRID_SIZE = 50;
+const SIDE_LENGTH = getSideLength();
+const grid = document.getElementById("grid");
 
 function getSideLength() {
     const main = document.querySelector("main");
     const WIDTH = Math.max(main.clientWidth, main.offsetWidth, main.scrollWidth); 
     const HEIGHT = Math.max(main.clientHeight, main.offsetHeight, main.scrollHeight);
     
-    const widthLength = WIDTH * 0.8 / 32;
-    const heightLength = HEIGHT * 0.8 / 32;
+    const widthLength = WIDTH * 0.8 / DEFAULT_GRID_SIZE;
+    const heightLength = HEIGHT * 0.8 / DEFAULT_GRID_SIZE;
     return Math.min(widthLength, heightLength);
 }
 
-const SIDE_LENGTH = getSideLength();
-console.log(SIDE_LENGTH);
-
-const grid = document.getElementById("grid");
-
-
-for (let row_index = 0; row_index < GRID_SIZE; row_index++) {
+function createRow(rowSize) {
     const row = document.createElement("div");
     row.style.display = "flex";
     
-    for (let column_index = 0; column_index < GRID_SIZE; column_index++) {
-        const cell = document.createElement("div");
-        cell.style.border = "1px solid black";
-        cell.style.width = `${SIDE_LENGTH}px`;
-        cell.style.height = `${SIDE_LENGTH}px`;
-        row.appendChild(cell);
+    for (let column_index = 0; column_index < rowSize; column_index++) {
+        row.appendChild(createCell());
     }
-    grid.appendChild(row);
+    return row;
 }
+
+function createCell() {
+    const cell = document.createElement("div");
+    cell.style.border = "0.1px solid grey";
+    cell.style.width = `${SIDE_LENGTH}px`;
+    cell.style.height = `${SIDE_LENGTH}px`;
+    cell.classList.add("cell");
+    cell.addEventListener("mouseenter", () => cell.style.backgroundColor = "blue");
+    return cell;
+}
+
+function createGrid(gridSize) {
+    for (let row_index = 0; row_index < gridSize; row_index++) {
+        grid.appendChild(createRow(gridSize));
+    }
+}
+
+function resetGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+const clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", () => {
+    gridSize = Number(prompt("Please enter size of the grid", "50"));
+    if (gridSize <= 100 && gridSize >=1) {
+        resetGrid();
+        createGrid(gridSize);
+    }
+    else {
+        alert(`Greed size must be from 1 to 100. You entered "${gridSize}"`);
+    }
+});
+
+createGrid(DEFAULT_GRID_SIZE);
